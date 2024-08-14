8086a6fcc493c5dd8b85fa660c051c1fcc7ae416
Richard Horton <157433386+Richard-013@users.noreply.github.com>
2024-08-14 23:02:45 +0100

    Solution to Valid Anagram

---
+++ ProgrammingSkills/ValidAnagram.cpp
@@ -0,0 +1,24 @@
+/*
+* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+* An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, using all the original letters exactly once.
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+
+        if( s == t )
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
