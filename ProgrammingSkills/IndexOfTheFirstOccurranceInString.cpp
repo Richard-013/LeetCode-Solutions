30fd3bc129f915f9793ca29f65c18312261c66c6
Richard Horton <157433386+Richard-013@users.noreply.github.com>
2024-08-14 20:19:06 +0100

    Create IndexOfTheFirstOccurranceInString.cpp

---
+++ ProgrammingSkills/IndexOfTheFirstOccurranceInString.cpp
@@ -0,0 +1,41 @@
+/*
+* Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int index = -1;
+        int needleLength = needle.length();
+        bool found = false;
+
+        for (int i = 0; i < haystack.length(); i++)
+        {
+            if(haystack[i] != needle[0])
+            {
+                continue;
+            }
+
+            for(int j = 0; j < needleLength; j++)
+            {
+                if(haystack[i+j] != needle[j])
+                {
+                    break;
+                }
+
+                if(j == needleLength - 1)
+                {
+                    found = true;
+                }
+            }
+
+            if(found)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+};
