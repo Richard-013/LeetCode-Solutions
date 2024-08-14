aaf1acf45137253a1335a7eda0acc8509b107bc6
Richard Horton <157433386+Richard-013@users.noreply.github.com>
2024-08-14 23:10:46 +0100

    Formatting update

--- ProgrammingSkills/ValidAnagram.cpp
+++ ProgrammingSkills/ValidAnagram.cpp
@@ -6,16 +6,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
-        {
+        if (s.length() != t.length()) {
             return false;
         }
 
         std::sort(s.begin(), s.end());
         std::sort(t.begin(), t.end());
 
-        if( s == t )
-        {
+        if (s == t) {
             return true;
         }
 
