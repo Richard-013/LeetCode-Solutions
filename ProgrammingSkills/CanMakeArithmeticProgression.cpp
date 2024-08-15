7d11a398d614cc60d5de3908acc9382ec986be6e
Richard Horton <157433386+Richard-013@users.noreply.github.com>
2024-08-15 18:18:01 +0100

    Insertion Sort solution for CanMakeArithmeticProgression

---
+++ ProgrammingSkills/CanMakeArithmeticProgression.cpp
@@ -0,0 +1,40 @@
+/*
+* A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+*
+* Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
+*/
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        short int arrSize = arr.size();
+
+        // Insertion Sort on data
+        int i = 1;
+
+        while (i < arrSize) {
+            int currentValue = arr[i];
+            int j = i;
+
+            while (j > 0 && arr[j - 1] > currentValue) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+
+            arr[j] = currentValue;
+            i++;
+        }
+
+        int initialDiff = arr[1] - arr[0];
+
+        for (int k = 2; k < arrSize; k++) {
+            int diff = arr[k] - arr[k - 1];
+
+            if (diff != initialDiff) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
